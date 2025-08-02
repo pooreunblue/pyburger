4e772559c768a26f1642adf9bb8fe39b1d6fce44
pooreunblue <mjbsh02@mju.ac.kr>
2025-08-02 19:29:37 +0900

    Feat: context에 객체 담아 template에 전달

--- pyburger/config/views.py
+++ pyburger/config/views.py
@@ -1,5 +1,6 @@
 # from django.http import HttpResponse
 from django.shortcuts import render
+from burgers.models import Burger
 
 def main(request):
     # return HttpResponse("안녕하세요, pyburger입니다.")
@@ -7,4 +8,9 @@ def main(request):
 
 def burger_list(request):
     # return HttpResponse("pyburger의 햄버거 목록입니다")
-    return render(request, "burger_list.html")
+    burgers = Burger.objects.all()
+    print("전체 햄버거 목록:", burgers)
+    context = {
+        "burgers": burgers,
+    }
+    return render(request, "burger_list.html", context)
